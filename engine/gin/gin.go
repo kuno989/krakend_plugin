ea162d0e25f181cd541423022e302c307c9ffe24
kuno989 <kuno@twelvelabs.io>
2021-11-02 20:24:15 +0900

    api gateway based

--- engine/gin/gin.go
+++ engine/gin/gin.go
@@ -8,8 +8,11 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/kuno989/cert_plugin"
 	"github.com/kuno989/cert_plugin/engine/pkg"
+	"github.com/kuno989/cert_plugin/engine/schema"
 	"net/http"
 )
+
+const HeaderAuthorization = "x-api-key"
 
 func Register(cfg *config.ServiceConfig, logger logging.Logger, engine *gin.Engine){
 	certCfg := cert_plugin.ParseConfig(cfg.ExtraConfig, logger)
@@ -21,19 +24,31 @@ func Register(cfg *config.ServiceConfig, logger logging.Logger, engine *gin.Engi
 	if err != nil {
 		fmt.Errorf("mongo connection")
 	}
-
 	engine.Use(middleware(mongo, logger))
 }
 
 func middleware(mongo *pkg.Mongo, logger logging.Logger) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		key, err := mongo.ApiKeyCheck(ctx, "123")
-		if err != nil {
-			logger.Error(fmt.Sprintf("%s 허용되지않은 라이센스 키"), key)
-			ctx.AbortWithStatus(http.StatusForbidden)
+		userToken := ctx.GetHeader(HeaderAuthorization)
+		if userToken == "" {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, schema.ResponseMessage{
+				Code: http.StatusUnauthorized,
+				Error:"Forbidden",
+				Message: "Forbidden",
+			})
 			return
 		}
-		fmt.Println("허용됨",key)
+
+		key, err := mongo.ApiKeyCheck(ctx, userToken)
+		if err != nil {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, schema.ResponseMessage{
+				Code: http.StatusUnauthorized,
+				Error:"Forbidden",
+				Message: "Forbidden",
+			})
+			return
+		}
+		fmt.Printf("허용된 api key %s\n",key.ApiKey)
 		ctx.Next()
 	}
 }
--- engine/schema/schema.go
+++ engine/schema/schema.go
@@ -1,9 +1,17 @@
 package schema
 
-type Key struct {
-	UserId    string `json:"user_id"`
-	CreatedAt string `json:"created_at"`
-	ExpiresAt string `json:"expires_at"`
-	ApiKey    string `json:"api_key"`
+import "go.mongodb.org/mongo-driver/bson/primitive"
 
+type Key struct {
+	ID        primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	UserId    string `json:"-"`
+	CreatedAt string `bson:"created_at" json:"created_at"`
+	ExpiresAt string `bson:"expires_at" json:"expires_at"`
+	ApiKey    string `bson:"api_key" json:"api_key"`
+}
+
+type ResponseMessage struct {
+	Code    int    `json:"code"`
+	Error   string `json:"error"`
+	Message string `json:"message"`
 }
